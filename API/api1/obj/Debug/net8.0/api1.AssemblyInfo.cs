5028810ed07ac5993eae09e5ed367bc670fb25f7
Laurinhadograu014 <131274111+Laurinhadograu014@users.noreply.github.com>
2024-04-03 15:20:29 -0300

    Correção Eventos
    Correção Eventos

--- API/api1/obj/Debug/net8.0/api1.AssemblyInfo.cs
+++ API/api1/obj/Debug/net8.0/api1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918f59821d3ec339e502ce3285f4e1f016e4ae96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6c2e7e1d503d3adbd853dda86589cef9b8daaa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("api1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
